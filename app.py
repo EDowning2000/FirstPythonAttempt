265ca498eef7fa36f21d10ab23ca9030b8c40c2e
EDowning2000 <emersondowning@gmail.com>
2020-08-26 18:40:23 -0400

    various helpful things like strings, numbers and an import

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+from math import *
 print('hello world')
+
+phrase="what is up world!"
+characterName = "emerson downing"
+characterAge="20"
+isMale=True
+newLine = '\n'
+checkToSeeIfUpper = isUpper()
+checkToSeeIfLower = isLower()
+checkLengthOfString = len()
+print('my name is ' + characterName +  ' i am ' +characterAge+' \n and im gonna learn to do a variable')
+print(phrase.upper())
